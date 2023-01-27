73cf6d933ca9491f644ba64731cd327dc9064cd9
Pavel <Qw3st@mail.ru>
2023-01-27 19:56:08 +0300

    .\DZ_041\

---
+++ DZ/DZ_041/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] CreateArray()
+{
+    Console.WriteLine("Какое количество чисел будем проверять?");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите число номер {i + 1}");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void SearchGreaterthanZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество числе больше 0 равно {count}");
+
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] matr = CreateArray();
+ShowArray(matr);
+SearchGreaterthanZero(matr);
+
+
